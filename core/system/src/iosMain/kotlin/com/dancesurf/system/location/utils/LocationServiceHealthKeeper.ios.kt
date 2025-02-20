f2c61c7fae6f83018957a41053db629df8586d5e
Denis Latushko <denis.latushko@cogniteq.com>
2025-02-20 22:41:21 +0100

    Fixed main thread lock when accessing location availability in ios

--- core/system/src/iosMain/kotlin/com/dancesurf/system/location/utils/LocationServiceHealthKeeper.ios.kt
+++ core/system/src/iosMain/kotlin/com/dancesurf/system/location/utils/LocationServiceHealthKeeper.ios.kt
@@ -4,20 +4,23 @@ import com.dancesurf.system.location.LocationError
 import com.dancesurf.system.permissions.Permission.ImpreciseLocation
 import com.dancesurf.system.permissions.Permission.PreciseLocation
 import com.dancesurf.system.permissions.PermissionChecker
-import com.dancesurf.system.permissions.PermissionStatus.Denied
-import com.dancesurf.system.permissions.delegate.LocationPermissionDelegate
 import com.dancesurf.system.permissions.fetchPermissionStatus
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import platform.CoreLocation.CLLocationManager
 
 actual class LocationServiceHealthKeeper(
     private val locationManager: CLLocationManager,
     private val permissionChecker: PermissionChecker
 ) {
-    actual suspend fun checkHealth(): LocationError? = when {
-        !hasLocationPermission() -> LocationError.PermissionDenied
-        !locationManager.locationServicesEnabled -> LocationError.ServiceNotEnabled
-        else -> null
-    }
+    actual suspend fun checkHealth(): LocationError? =
+        withContext(Dispatchers.Default) {
+            when {
+                !hasLocationPermission() -> LocationError.PermissionDenied
+                !locationManager.locationServicesEnabled -> LocationError.ServiceNotEnabled
+                else -> null
+            }
+        }
 
     actual suspend fun hasLocationPermission(): Boolean {
         val hasCoarseLocationPermission = permissionChecker.fetchPermissionStatus(ImpreciseLocation)
